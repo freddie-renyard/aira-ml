d5b7197043478a1c3baddca3b348f771a509cfe2
Freddie Renyard <freddierenyard@hotmail.co.uk>
2022-02-21 20:34:40 +0000

    Added more unit tests for the unsigned compiler

--- tests/test_binary_tools.py
+++ tests/test_binary_tools.py
@@ -3,3 +3,8 @@ from aira_ml.tools.binary_tools import BinCompiler
 def test_unsigned():
     assert BinCompiler.compile_to_uint(0.5, 3, 3) == "100"
     assert BinCompiler.compile_to_uint(1,8,0) == "00000001"
+    assert BinCompiler.compile_to_uint(1.125, 4, 3) == "1001"
+    assert BinCompiler.compile_to_uint(255, 8, 0) == "11111111"
+    assert BinCompiler.compile_to_uint(15.0, 8, 4) == "11110000"
+    assert BinCompiler.compile_to_uint(85, 7, 0) == "1010101"
+    assert BinCompiler.compile_to_uint(0.0000001, 4, 4) == "0000"
