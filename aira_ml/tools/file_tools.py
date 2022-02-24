5f402a5e1b727bcb0f935f99ea50851e7dfb5d52
Freddie Renyard <freddierenyard@hotmail.co.uk>
2022-02-24 10:08:45 +0000

    Added file caching functionality to the file tools

--- aira_ml/tools/file_tools.py
+++ aira_ml/tools/file_tools.py
@@ -1,7 +1,36 @@
+import os 
 
 class Filetools:
 
     @classmethod
-    def save_output_file():
-        """Saves a list of binary strings in a .mem file to the cache.
+    def save_to_file(cls, filename, target_list):
+        """ Saves a specified list to a .mem file in the cache with entry comments.
+        
+        Parameters
+        ----------
+        filename: str
+            The desired filename.
+        target_list: [str]
+            The binary parameters supplied as a list of strings.
         """
+
+        path = cls.open_cache(filename)
+        print(path)
+        file = open(path, "w")
+
+        for i, element in enumerate(target_list):
+            file.write("//" + "Entry " + str(i) + '\n')
+            file.write(element + " " + '\n')
+
+    @staticmethod
+    def open_cache(filename):
+        # Obtain path to cache
+        path = os.path.realpath(__file__)
+        path = path.replace("/tools", "")
+        dir = os.path.dirname(path) + "/cache"
+
+        # Create the cache if it does not exist
+        if not os.path.exists(dir):
+            os.makedirs(dir)
+        
+        return str(dir) + "/" + filename + ".mem"
