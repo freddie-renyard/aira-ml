cef2c89411d9715540453f8786ec580d66f2c8bf
Freddie Renyard <freddierenyard@hotmail.co.uk>
2022-02-24 09:59:45 +0000

    Added look up table compiler for exponentials

--- aira_ml/tools/lut_compiler.py
+++ aira_ml/tools/lut_compiler.py
@@ -1,11 +1,11 @@
 from aira_ml.tools.binary_tools import BinCompiler
-from math import exp, ceil, log2
+from math import exp
 
 class LUTCompiler:
 
     @classmethod
     def compile_sigmoid(cls, n_mantissa_in, n_exp_in, n_output):
-        """ Compile an lookup table for a sigmoid function with
+        """ Compiles an lookup table for a sigmoid function with
         floating point inputs and unsigned fixed point outputs.
 
         Returns a sorted list of binary strings with addresses
@@ -31,5 +31,31 @@ class LUTCompiler:
         out_sigmoid_vals = [sigmoid(x) for x in in_float_vals]
 
         out_bin = [BinCompiler.compile_to_uint(x, n_output, n_output) for x in out_sigmoid_vals] 
+
+        return out_bin
+    
+    @classmethod
+    def compile_exponential(cls, n_mantissa_in, n_exp_in, n_mantissa_out, n_exp_out):
+        """ Compiles an lookup table for the exponential function with
+        floating point inputs and floating point outputs.
+
+        Returns a sorted list of binary strings with addresses
+        corresponding to the input data values.
+        """
+        
+        n_input = 1 + n_mantissa_in + n_exp_in
+        lut_res = 2 ** n_input
+
+        # Determine all the possible input binary numbers
+        in_bin_str = [BinCompiler.compile_to_uint(x, n_input, 0) for x in range(0, lut_res)]
+
+        # Determine what value this binary actually represents under 
+        # floating point interpretation.
+        in_float_vals = [BinCompiler.decode_custom_float(x, n_mantissa_in, n_exp_in) for x in in_bin_str]
+
+        # Compute the sigmoid value for each value.
+        out_exp_vals = [exp(x) for x in in_float_vals]
+
+        out_bin = [BinCompiler.compile_to_float(x, n_mantissa_out, n_exp_out) for x in out_exp_vals] 
 
         return out_bin
