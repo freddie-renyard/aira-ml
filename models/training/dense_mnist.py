561a7c34b61ce7730e4c3153a840ec1411b7ab61
Freddie Renyard <freddierenyard@hotmail.co.uk>
2022-02-25 19:52:01 +0000

    Modified test model architecture

--- models/training/dense_mnist.py
+++ models/training/dense_mnist.py
@@ -13,7 +13,8 @@ x_test = x_test.astype("float32") / 255.0
 
 model = tf.keras.models.Sequential([
   tf.keras.layers.Flatten(input_shape=(28, 28)),
-  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dense(50, activation='relu'),
+  tf.keras.layers.Dense(50, activation='relu'),
   tf.keras.layers.Dense(10, activation='relu')
 ])
 
