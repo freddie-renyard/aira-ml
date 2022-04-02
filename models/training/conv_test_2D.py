7af0b1d786c18718be6ad36be933d2a200fd529a
Freddie Renyard <freddierenyard@hotmail.co.uk>
2022-04-02 09:42:03 +0100

    Added a single layer convolution model to test RTL with

---
+++ models/training/conv_test_2D.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten
+
+# Construct a simple 2D convolutional model for testing.
+input_shape = (9,9,1)
+model = tf.keras.models.Sequential([
+    Conv2D(6, (3,3), padding='valid', activation='relu', input_shape=input_shape)
+])
+
+model.compile(
+    optimizer=tf.keras.optimizers.Adam(0.001),
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=[tf.keras.metrics.SparseCategoricalAccuracy()],
+)
+
+model.summary()
+
+model.save('models/conv_test_2D/model')
